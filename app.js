4a3bb8fcb58ac66010759766c2aa64cd9a78f6a4
Bohdan Pantiukhov <58185639+gingermanymph@users.noreply.github.com>
2020-05-31 17:25:15 +0300

    Add files via upload
    Init

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const PORT = process.env.PORT || 808;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+    const rMethod = req.method;
+    const rUrl = req.url;
+    const rHeaders = req.headers;
+    console.info('START-------------------------------------------------------------\n'+rMethod + ' ' + rUrl);
+    console.info(rHeaders);
+    console.info('BODY--------------------------------------------------------------\n'+JSON.stringify(req.body));
+    res.statusCode = 200;
+    res.send();
+    console.info('END---------------------------------------------------------------\n\n\n');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is working on ${PORT}`);
+});
